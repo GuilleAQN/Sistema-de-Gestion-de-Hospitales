0b494e5bf691d6ec3202347f0c5b0b36ca42f0ab
GuilleAQN <omarguillermo1@gmail.com>
2024-07-10 16:20:43 -0400

    feat(api): Incluir Middleware general para errores

---
+++ API/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace API.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Una excepción no manejada ha ocurrido.");
+                await ManejarExcepcionAsync(context, ex);
+            }
+        }
+
+        private static Task ManejarExcepcionAsync(HttpContext context, Exception exception)
+        {
+            var problemDetails = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Un error inesperado ha ocurrido!",
+                Detail = exception.Message,
+                Instance = context.Request.Path
+            };
+
+            context.Response.ContentType = "application/problem+json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var json = JsonSerializer.Serialize(problemDetails);
+            return context.Response.WriteAsync(json);
+        }
+    }
+}
