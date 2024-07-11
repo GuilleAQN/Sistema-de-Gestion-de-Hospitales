4d650170f34b9c755cba666f3a6a76b299e0ca26
GuilleAQN <omarguillermo1@gmail.com>
2024-07-11 11:05:07 -0400

    feat(front): Incluir MudBlazor

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -1,11 +1,17 @@
 using FrontEnd;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+builder.Services.AddScoped(sp =>
+    new HttpClient
+    {
+        BaseAddress = new Uri("https://localhost:5432")
+    });
 
 await builder.Build().RunAsync();
