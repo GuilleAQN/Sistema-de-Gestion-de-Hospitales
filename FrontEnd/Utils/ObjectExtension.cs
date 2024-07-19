127509f5f64805b6172a66e80fb27f5db4c7a9cd
GuilleAQN <omarguillermo1@gmail.com>
2024-07-19 12:59:22 -0400

    feat(front): Añadir convertidor de DTOs de Get para Update

---
+++ FrontEnd/Utils/ObjectExtension.cs
@@ -0,0 +1,17 @@
+﻿public static class ObjectExtensions
+{
+    public static void CopyPropertiesTo<T, U>(this T source, U destination)
+    {
+        var sourceProperties = typeof(T).GetProperties();
+        var destinationProperties = typeof(U).GetProperties();
+
+        foreach (var sourceProperty in sourceProperties)
+        {
+            var destinationProperty = destinationProperties.FirstOrDefault(p => p.Name == sourceProperty.Name && p.PropertyType == sourceProperty.PropertyType);
+            if (destinationProperty != null)
+            {
+                destinationProperty.SetValue(destination, sourceProperty.GetValue(source, null), null);
+            }
+        }
+    }
+}
