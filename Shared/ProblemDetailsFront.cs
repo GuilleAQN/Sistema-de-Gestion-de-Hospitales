842eca1d6e2e44638cf3267811ffb396563f4a9d
GuilleAQN <omarguillermo1@gmail.com>
2024-07-19 13:01:03 -0400

    feat(front): Añadir clase de ProblemDetail para poder devolver los errores del BackEnd

---
+++ Shared/ProblemDetailsFront.cs
@@ -0,0 +1,11 @@
+﻿namespace Sistema_de_Gestion_de_Hospitales.Shared
+{
+    public class ProblemDetailsFront
+    {
+        public string? Type { get; set; }
+        public int Status { get; set; }
+        public string? Title { get; set; }
+        public string? Detail { get; set; }
+        public string? Instance { get; set; }
+    }
+}
